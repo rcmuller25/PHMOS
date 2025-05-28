eafe176f7cff70a4173b36a8baac5b5dda34f291
Canzuis <80487347+rcmuller25@users.noreply.github.com>
2025-05-28 11:47:31 +0200

    Update Metro configuration for proper module handling

--- metro.config.js
+++ metro.config.js
@@ -4,4 +4,8 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+// Add support for handling modules properly
+config.resolver.sourceExts.push('cjs');
+config.resolver.assetExts = ['ttf', 'woff', 'woff2', 'eot', 'svg', 'png', 'jpg', 'jpeg', 'gif', 'ico'];
+
 module.exports = config;
